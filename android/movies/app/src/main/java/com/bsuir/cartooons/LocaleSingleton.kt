7f7496835f5d1c206c4d2d0da308a0b20ae41c3b
Denis Avko <actualdennis@gmail.com>
2020-03-02 20:52:26 +0300

    ADD BY Language

---
+++ android/movies/app/src/main/java/com/bsuir/cartooons/LocaleSingleton.kt
@@ -0,0 +1,4 @@
+package com.bsuir.cartooons
+
+class LocaleSingleton {
+}
---
+++ android/movies/app/src/main/java/com/bsuir/cartooons/settings_activity.kt
@@ -0,0 +1,12 @@
+package com.bsuir.cartooons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class settings_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_activity)
+    }
+}
